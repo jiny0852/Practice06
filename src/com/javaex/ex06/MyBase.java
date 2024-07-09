3c1d7ed8f5c6b8f02002e95128d3f1d99778de24
jiny0852 <jiny0852@gmail.com>
2024-07-09 10:06:10 +0900

    2024-07-09 과제완

--- src/com/javaex/ex06/MyBase.java
+++ src/com/javaex/ex06/MyBase.java
@@ -6,12 +6,15 @@ public class MyBase extends Base{
 	
 	public void service(String state) {
 
-        if (state.equals("오후")) { //문자열의 같다(==) 는 equals()를 사용합니다.
+        if (state.equals("낮")) { //문자열의 같다(==) 는 equals()를 사용합니다.
+        	day();
+        }
+        else if (state.equals("밤")) {
+        	night();
+        }
+        else if (state.equals("오후")) {
         	afternoon();
         }
-        /*else if (state.equals("오후")) {
-        	afternoon();
-        }*/
     }
 	
 	public void day() {
--- src/com/javaex/ex07/SmartPhone.java
+++ src/com/javaex/ex07/SmartPhone.java
@@ -3,15 +3,28 @@ package com.javaex.ex07;
 public class SmartPhone extends MusicPhone {
     
     public void execute(String str){
+    	
+    	
+    	//코드작성
+    	if ("음악".equals(str)){
+            playMusic();
+        } else if ("앱".equals(str)) {
+        	playApp();
+        } else if ("통화".equals(str)) {
+        	super.execute(str);
+        }
         
-        //코드작성
         
     }
  
     //메소드작성
-    
+    protected void playMusic(){
+        System.out.println("다운로드에서 음악재생");
+    }
     //메소드작성
-    
+    protected void playApp(){
+        System.out.println("앱실행");
+    }
     
     
 }
